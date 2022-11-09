fab995244718edbbcb1d7664fb8bad9110c31246
Allan COSTES <allancts.compte@gmail.com>
2022-11-09 23:57:50 +0100

    Update login.php

--- Allan_Files/login.php
+++ Allan_Files/login.php
@@ -1,10 +1,11 @@
-<form action="accueil.php" method="POST">
-
 <?php $var = 1; ?>
 
+<form action="accueil.php" method="POST">
 <input type="hidden" name="idCompte" value = <?php echo $var; ?>>
-<button type='submit'> CONNECT </button>
+<button type='submit'> ACCUEIL </button>
+</form>
 
-
-
+<form action="accueil.php" method="POST">
+<input type="hidden" name="idCompte" value = <?php echo $var; ?>>
+<button type='submit'> CREATE FICHE </button>
 </form>
